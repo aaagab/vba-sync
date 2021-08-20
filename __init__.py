7df1f25fcefef728e45cb607fea35c8d8e1864a0
gabaaa <gabaaa.dev@gmail.com>
2021-08-20 08:20:31 -0500

    first commit after clone

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: vba-sync
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.vba-sync import vba-sync
+# from .gpkgs import message as msg
+# from .gpkgs.options import Options
+# from .gpkgs.etconf import Etconf
