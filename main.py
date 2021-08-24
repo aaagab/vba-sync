744b008a199e69aab5cd2782ad24302e0819afa9
gabaaa <gabaaa.dev@gmail.com>
2021-08-24 10:29:43 -0500

    removed module etconf and put the cache file in srcs folder

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ if __name__ == "__main__":
     pkg = importlib.import_module(module_name)
     del sys.path[0]
 
-    def seed(pkg_major, direpas_configuration=dict(), fun_auto_migrate=None):
-        fun_auto_migrate()
-    etconf=pkg.Etconf(enable_dev_conf=False, tree=dict( files=dict({ "settings.json": dict() })), seed=seed)
     args, dy_app=pkg.Options(
-        direpa_configuration=etconf.direpa_configuration,
         examples=r"""
             main.py --export --workbook ..\test\generate-interactions-report.xlsm
             main.py --import --workbook "..\test\generate-interactions report.xlsm" --overwrite
@@ -37,10 +33,6 @@ if __name__ == "__main__":
              os.system('rmdir /S /Q "{}"'.format(direpa_recovery))
         os.makedirs(direpa_recovery, exist_ok=True)
 
-    direpa_tmp=os.path.join(os.path.expanduser("~"), "fty", "tmp", dy_app["name"][0], dy_app["name"])
-    os.makedirs(direpa_tmp, exist_ok=True)
-    filenpa_cache=os.path.join(direpa_tmp, "cache.json")
-
     if args.export.here or args._import.here or args.macro.here:
         direpa_srcs=args.srcs.value
         filenpa_workbook=args.workbook.value
@@ -48,6 +40,8 @@ if __name__ == "__main__":
         if direpa_srcs is None:
             filer_workbook, ext=os.path.splitext(filenpa_workbook)
             direpa_srcs=os.path.join(os.path.dirname(filenpa_workbook), os.path.basename(filer_workbook).replace(" ", "_").lower())
+
+        filenpa_cache=os.path.join(direpa_srcs, "vba-sync-cache.json")
 
         if args.export.here:
             pkg.export(
@@ -57,16 +51,12 @@ if __name__ == "__main__":
             )
             sys.exit(0)
         elif args._import.here:
-            # keep_open=False
-            # if args.macro.here:
-            #     keep_open=True
             pkg._import(
                 filenpa_cache,
                 filenpa_workbook=filenpa_workbook,
                 direpa_srcs=direpa_srcs,
                 overwrite=args.overwrite.here,
                 reset_cache=args.reset_cache.here,
-                # keep_open=keep_open,
             )
 
         if args.macro.here:
